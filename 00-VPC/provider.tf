04272ff653d318ab2f11969c1f46244abb24edb3
Nagaraj <nagarajuande255@gmail.com>
2025-06-23 19:42:12 +0530

    terraform

---
+++ 00-VPC/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.98.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "devops-84s" # This should match the bucket in your tfvars files
+    key          = "roboshop-infra-dev"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1" # Specify the AWS region
+}
