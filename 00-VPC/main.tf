cf4daac66539c1c6e73361dc1684b5d24a52339c
Nagaraj <nagarajuande255@gmail.com>
2025-06-23 20:45:50 +0530

    terraform

--- 00-VPC/main.tf
+++ 00-VPC/main.tf
@@ -1,10 +1,10 @@
 module "vpc" {
-  source = "git::https://github.com/Nagaraj411/vpc-module-test.git?ref=main"
+  source = "git::https://github.com/Nagaraj411/terraform-aws-vpc.git?ref=main"  # must match with module source with terraform-aws-vpc
   project               = var.project
   environment           = var.environment
   public_subnet_cidrs   = var.public_subnet_cidrs
   private_subnet_cidrs  = var.private_subnet_cidrs
   database_subnet_cidrs = var.database_subnet_cidrs
+}
 
   #is_peering_required = true
-}
