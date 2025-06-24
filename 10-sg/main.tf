35940fc4f3c646564f24c94628aea05b29316a10
Nagaraj <nagarajuande255@gmail.com>
2025-06-24 16:08:57 +0530

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -5,5 +5,5 @@ module "frontend"{
     environment = var.environment
     sg_name = var.frontend_sg_name
     sg_description = var.frontend_sg_description
-    vpc_id = data.aws_ssm_parameter.vpc_id.value # if u want to create local variable, you can use local.vpc_id
+    vpc_id = local.vpc_id # if u want to create local variable, you can use local.vpc_id
 }
