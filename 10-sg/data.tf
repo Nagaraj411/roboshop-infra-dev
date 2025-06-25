1e6fc95382928926be8cf97e1b948db0c04a569c
Nagaraj <nagarajuande255@gmail.com>
2025-06-25 12:46:45 +0530

    terraform

--- 10-sg/data.tf
+++ 10-sg/data.tf
@@ -3,3 +3,13 @@ data "aws_ssm_parameter" "vpc_id" {
 }
 
 # this script retrieves the VPC ID from SSM parameter store for the project and environment sent by VPC group 
+
+data "aws_ssm_parameter" "public_subnet_ids" {
+    name        = "/${var.project}/${var.environment}/public_subnet_ids"
+}
+# this script retrieves the public subnet IDs from SSM parameter store for the project and environment sent by VPC group
+
+data "aws_ssm_parameter" "bastion_sg_id" {
+    name        = "/${var.project}/${var.environment}/bastion_sg_id"
+}
+# this script retrieves the bastion security group ID from SSM parameter store for the project and environment sent by SG group
--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -2,10 +2,11 @@
     ami = local.ami_id
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.bastion_sg_id]
-    subnet_id = module.vpc.public_subnets[0] # use the first public subnet for the bastion host
+    subnet_id = local.public_subnet_id # use the first public subnet for the bastion host
 
     tags = {
-      Name = "roboshop-dev-bastion"
-    }
-}
+        Name = "${var.project}-${var.environment}-bastion"
+      }
+
+ }
  
