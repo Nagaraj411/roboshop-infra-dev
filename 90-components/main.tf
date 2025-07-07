25addd915092f338e14347e30b246c3f13b34f4f
Nagaraj <nagarajuande255@gmail.com>
2025-07-08 01:37:12 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,7 @@
 module "component" {
   for_each     = var.components
-  source       = "git::https://github.com/Nagaraj411/roboshop-terraform-ansible-test.git?ref=main"
+  # source = "git::https://github.com/Nagaraj411/terraform-aws-roboshop-module.git?ref=main"
+  source       = "../../terraform-aws-roboshop-module"
   component    = each.key
   rule_priority = each.value.rule_priority
 }
