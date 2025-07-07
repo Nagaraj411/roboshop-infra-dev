b0f9f5eefdb2b9e82fb0cde6cdcb8d10a1a5f21c
Nagaraj <nagarajuande255@gmail.com>
2025-07-07 12:19:38 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -38,118 +38,118 @@ resource "terraform_data" "mongodb" { # This resource is used to manage the Mong
 
 # redis instance configuration with ansible for roboshop project
 # This file creates a Redis instance in AWS using Terraform & provisions it with a bootstrap script running on the instance.
-# resource "aws_instance" "redis" {
-#   ami                    = local.ami_id
-#   instance_type          = "t3.micro"
-#   vpc_security_group_ids = [local.redis_sg_id]
-#   subnet_id              = local.database_subnet_ids
+resource "aws_instance" "redis" {
+  ami                    = local.ami_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [local.redis_sg_id]
+  subnet_id              = local.database_subnet_ids
 
-#   tags = merge(
-#     local.common_tags,
-#     {
-#       Name = "${var.project}-${var.environment}-redis"
-#     }
-#   )
-# }
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-redis"
+    }
+  )
+}
 
-# resource "terraform_data" "redis" { # This resource is used to manage the Redis instance
-#   triggers_replace = [
-#     aws_instance.redis.id # This is used to trigger the resource to be replaced when the Redis instance is updated 
-#   ]
-#   provisioner "file" {
-#     source      = "bootstrap.sh"
-#     destination = "/tmp/bootstrap.sh"
-#   }
-#   connection {
-#     type     = "ssh"
-#     user     = "ec2-user"
-#     password = "DevOps321"
-#     host     = aws_instance.redis.private_ip
-#   }
-#   provisioner "remote-exec" {
-#     inline = [
-#       "chmod +x /tmp/bootstrap.sh",
-#       "sudo sh /tmp/bootstrap.sh redis ${var.environment}" # redis represent as($1) is the argument passed to the script & redis-dev
-#     ]
-#   }
-# }
+resource "terraform_data" "redis" { # This resource is used to manage the Redis instance
+  triggers_replace = [
+    aws_instance.redis.id # This is used to trigger the resource to be replaced when the Redis instance is updated 
+  ]
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.redis.private_ip
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh redis ${var.environment}" # redis represent as($1) is the argument passed to the script & redis-dev
+    ]
+  }
+}
 
-# # mysql instance configuration with ansible for roboshop project
-# # This file creates a mysql instance in AWS using Terraform & provisions it with a bootstrap script running on the instance.
-# resource "aws_instance" "mysql" {
-#   ami                    = local.ami_id
-#   instance_type          = "t3.micro"
-#   vpc_security_group_ids = [local.mysql_sg_id]
-#   subnet_id              = local.database_subnet_ids
-#   iam_instance_profile   = "EC2RoleToFetchSMM" # This will create and attach the IAM role to the instance for fetching secrets from AWS Secrets Manager
+# mysql instance configuration with ansible for roboshop project
+# This file creates a mysql instance in AWS using Terraform & provisions it with a bootstrap script running on the instance.
+resource "aws_instance" "mysql" {
+  ami                    = local.ami_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [local.mysql_sg_id]
+  subnet_id              = local.database_subnet_ids
+  iam_instance_profile   = "EC2RoleToFetchSMM" # This will create and attach the IAM role to the instance for fetching secrets from AWS Secrets Manager
 
-#   tags = merge(
-#     local.common_tags,
-#     {
-#       Name = "${var.project}-${var.environment}-mysql"
-#     }
-#   )
-# }
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-mysql"
+    }
+  )
+}
 
-# resource "terraform_data" "mysql" { # This resource is used to manage the mysql instance
-#   triggers_replace = [
-#     aws_instance.mysql.id # This is used to trigger the resource to be replaced when the mysql instance is updated 
-#   ]
-#   provisioner "file" {
-#     source      = "bootstrap.sh"
-#     destination = "/tmp/bootstrap.sh"
-#   }
-#   connection {
-#     type     = "ssh"
-#     user     = "ec2-user"
-#     password = "DevOps321"
-#     host     = aws_instance.mysql.private_ip
-#   }
-#   provisioner "remote-exec" {
-#     inline = [
-#       "chmod +x /tmp/bootstrap.sh",
-#       "sudo sh /tmp/bootstrap.sh mysql ${var.environment}" # mysql represent as($1) is the argument passed to the script & mysql-dev
-#     ]
-#   }
-# }
+resource "terraform_data" "mysql" { # This resource is used to manage the mysql instance
+  triggers_replace = [
+    aws_instance.mysql.id # This is used to trigger the resource to be replaced when the mysql instance is updated 
+  ]
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mysql.private_ip
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh mysql ${var.environment}" # mysql represent as($1) is the argument passed to the script & mysql-dev
+    ]
+  }
+}
 
-# # rabbitmq instance configuration with ansible for roboshop project
-# # This file creates a rabbitmq instance in AWS using Terraform & provisions it with a bootstrap script running on the instance.
-# resource "aws_instance" "rabbitmq" {
-#   ami                    = local.ami_id
-#   instance_type          = "t3.micro"
-#   vpc_security_group_ids = [local.rabbitmq_sg_id]
-#   subnet_id              = local.database_subnet_ids
+# rabbitmq instance configuration with ansible for roboshop project
+# This file creates a rabbitmq instance in AWS using Terraform & provisions it with a bootstrap script running on the instance.
+resource "aws_instance" "rabbitmq" {
+  ami                    = local.ami_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
+  subnet_id              = local.database_subnet_ids
 
-#   tags = merge(
-#     local.common_tags,
-#     {
-#       Name = "${var.project}-${var.environment}-rabbitmq"
-#     }
-#   )
-# }
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-rabbitmq"
+    }
+  )
+}
 
-# resource "terraform_data" "rabbitmq" { # This resource is used to manage the rabbitmq instance
-#   triggers_replace = [
-#     aws_instance.rabbitmq.id # This is used to trigger the resource to be replaced when the rabbitmq instance is updated 
-#   ]
-#   provisioner "file" {
-#     source      = "bootstrap.sh"
-#     destination = "/tmp/bootstrap.sh"
-#   }
-#   connection {
-#     type     = "ssh"
-#     user     = "ec2-user"
-#     password = "DevOps321"
-#     host     = aws_instance.rabbitmq.private_ip
-#   }
-#   provisioner "remote-exec" {
-#     inline = [
-#       "chmod +x /tmp/bootstrap.sh",
-#       "sudo sh /tmp/bootstrap.sh rabbitmq ${var.environment}" # rabbitmq represent as($1) is the argument passed to the script & rabbitmq-dev
-#     ]
-#   }
-# }
+resource "terraform_data" "rabbitmq" { # This resource is used to manage the rabbitmq instance
+  triggers_replace = [
+    aws_instance.rabbitmq.id # This is used to trigger the resource to be replaced when the rabbitmq instance is updated 
+  ]
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.rabbitmq.private_ip
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh rabbitmq ${var.environment}" # rabbitmq represent as($1) is the argument passed to the script & rabbitmq-dev
+    ]
+  }
+}
 
 # Creating route 53 Record for mongodb
 resource "aws_route53_record" "mongodb" {
@@ -162,32 +162,32 @@ resource "aws_route53_record" "mongodb" {
 }
 
 
-# # Creating route 53 Record for redis
-# resource "aws_route53_record" "redis" {
-#   zone_id = var.zone_id
-#   name    = "redis-${var.environment}.${var.zone_name}" #redis-dev.devops84.shop
-#   type    = "A"
-#   ttl     = 1
-#   records = [aws_instance.redis.private_ip]
-#   allow_overwrite = true
-# }
+# Creating route 53 Record for redis
+resource "aws_route53_record" "redis" {
+  zone_id = var.zone_id
+  name    = "redis-${var.environment}.${var.zone_name}" #redis-dev.devops84.shop
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
+}
 
-# # Creating route 53 Record for mysql
-# resource "aws_route53_record" "mysql" {
-#   zone_id = var.zone_id
-#   name    = "mysql-${var.environment}.${var.zone_name}" #mysql-dev.devops84.shop
-#   type    = "A"
-#   ttl     = 1
-#   records = [aws_instance.mysql.private_ip]
-#   allow_overwrite = true
-# }
+# Creating route 53 Record for mysql
+resource "aws_route53_record" "mysql" {
+  zone_id = var.zone_id
+  name    = "mysql-${var.environment}.${var.zone_name}" #mysql-dev.devops84.shop
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.mysql.private_ip]
+  allow_overwrite = true
+}
 
-# # Creating route 53 Record for rabbitmq
-# resource "aws_route53_record" "rabbitmq" {
-#   zone_id = var.zone_id
-#   name    = "rabbitmq-${var.environment}.${var.zone_name}" #rabbitmq-dev.devops84.shop
-#   type    = "A"
-#   ttl     = 1
-#   records = [aws_instance.rabbitmq.private_ip]
-#   allow_overwrite = true
-# }
+# Creating route 53 Record for rabbitmq
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = var.zone_id
+  name    = "rabbitmq-${var.environment}.${var.zone_name}" #rabbitmq-dev.devops84.shop
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.rabbitmq.private_ip]
+  allow_overwrite = true
+}
