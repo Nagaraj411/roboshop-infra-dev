6b5b9bc1ddc42fa6226c764fbc1c400025e6d3f6
Nagaraj <nagarajuande255@gmail.com>
2025-07-08 09:03:55 +0530

    terraform

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -1,20 +1,20 @@
 variable "components" {
   default = {
-    
+
     user = {
         rule_priority = 20
     }
-    cart = {
-        rule_priority = 30
-    }
-    shipping = {
-      rule_priority = 40
-    }
-    payment = {
-      rule_priority = 50
-    }
-    frontend = {
-      rule_priority = 10
-    }
+  #   cart = {
+  #       rule_priority = 30
+  #   }
+  #   shipping = {
+  #     rule_priority = 40
+  #   }
+  #   payment = {
+  #     rule_priority = 50
+  #   }
+  #   frontend = {
+  #     rule_priority = 10
+  #   }
   }
 }
